db5f8b2f33bb3ad1b5f68433d1edcafc25732257
Ivan <ivanchernetskey@mail.ru>
2020-11-10 16:19:18 +0300

    files added

---
+++ src/bsu/rfe/java/group10/lab1/Charnetsky/varC3/Main.java
@@ -0,0 +1,12 @@
+package bsu.rfe.java.group10.lab1.Charnetsky.varC3;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
